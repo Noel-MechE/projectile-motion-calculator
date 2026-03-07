07a5d0999b1e854498e597b18e069f5ebc5c414b
Noel-MechE <artemisdragon06@gmail.com>
2026-03-06 20:58:17 -0800

    Add files via upload

---
+++ Projectile.cpp
@@ -0,0 +1,52 @@
+/*
+    This program calculates projectile motion
+    Written by: Noel A Bobadilla Castillo
+    Date: March 2026
+*/
+
+//Since we will be accpeting inputs and displaying outputs we must work with the following library 
+#include <iostream>
+//Since we will also be working with math functions we will using cmath
+#include <cmath> //Remeber to convert from degrees to radians
+#include <iomanip> //Will be included for the use of setprecision and fixed manipulators
+
+using namespace std;
+
+int main() {
+
+    //The program will accept two inputs from the user that being velocity anf angle so let us declare these variables from the user
+    double Velocity;
+    double Angle;
+
+/*Since most physics values involve decimals we declare
+    variables as double to accept decimal numbers.
+    Input variables are not given a value because those
+    will come from the user. The constants G and PI are
+ declared and unchanged throughout the program.*/
+
+    //Since this is a physics based program, we will always work with certain constants and in this case we will work with gravity and pi, therefore let us declare those constants
+    const double G = 9.81;
+    /*
+    Once we have declared variables and the things we will acpet and work with we can now begin to accept such inputs
+    */
+    
+    //User input
+    cout<< "What is the launch angle: ";
+    cin>> Angle;
+
+    cout<< "What is the intial velocity (m/s): ";
+    cin>> Velocity;
+
+    /*Recalling that cmath works with radians our next step 
+    before making calulations is to ensure all values are in 
+    their proper units
+    */
+   //Converting angles from degree to radians
+   double AngleRad = Angle * (M_PI / 180.0);
+    
+//Once we have the required data/info we will procced to carry out the calculations
+double TimeOfFlight = (2*Velocity*sin(AngleRad))/ G;
+double MaxHeight = ((Velocity*Velocity)*(sin(AngleRad)*sin(AngleRad)))/(2*G);
+double HorizontalRange = ((Velocity*Velocity)*sin(2*AngleRad))/G;
+    return 0;
+}
